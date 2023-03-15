a40aa2dbe76346e9e0bac81d66aa4bfe4c898ea7
-dav <davtyan.arman1996@gmail.com>
2023-03-15 23:04:07 +0400

    no commits

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import One from "./Components/One"
+import Clock from "./Components/Clock";
+import Test from "./Components/Test"
+import Calc from "./Components/calculator";
+import Breeds from "./Components/Breeds";
+
+
+
+function App() {
+  return (
+  <div> 
+    <Clock />
+      <Breeds />
+      
+    </div>
+  )
+}
+
+export default App   
